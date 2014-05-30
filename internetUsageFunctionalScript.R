bb799a62f02b3e8f9468bd71c9c5919d3875c244
Travis <travismay@gmail.com>
2014-05-30 16:42:24 -0500

    Modify internet usage functions

--- internetUsageFunctionalScript.R
+++ internetUsageFunctionalScript.R
@@ -1,24 +1,41 @@
+#### Noob functional method for handling internet usage data via the FRED API.
 #### Source internet usage data via FRED API because quantmod FRED functionality sucks.
 
-##First, use httr library for GET functions
+##Use httr library for GET functions and jsonlite library to handle json data.
 library(httr)
+library(jsonlite)
 
 ## Establish API URL request by indiviual query strings
 apiKey <- '76bb1186e704598b725af0a27159fdfc'
-apiString <- '&api_key'
-baseUrl <- 'http://api.stlouisfed.org/fred/release/series?release_id='
-releaseId <- '234'
+apiString <- '&api_key='
+baseUrl <- 'https://api.stlouisfed.org/fred/release/series?release_id='
 fileTypeString <- '&file_type=json'
-fullUrl <- paste(baseUrl,releaseId,apiString,fileTypeString,sep="")
+releaseUrl <- paste(baseUrl,releaseId,apiString,apiKey,fileTypeString,sep="")
 
-release <- GET(fullUrl)
+## Get release information
+release <- GET(releaseUrl)
 
 ## Extract internet usage data
+json1 <- content(release)
+json2 <- jsonlite::fromJSON(toJSON(json1))
+json3 <- json2$seriess
+json4 <- json3[grepl("ITNET",json3$id),]
+json5 <- cbind(json4$id,json4$title)
+colnames(json5) <- c('id', 'title')
+
+aseanCtry <- c("Thailand", "Myanmar", "Cambodia", "Laos", "Malaysia", "Vietnam", "Indonesia", "Singapore", "Philippines", "Brunei")
+
+getSeriesIds <- function(list) {
+  function (x) {
+    
+  }
+}
+
 
 ## Calculate world average
 
 ## Create percentiles of world data
 
-## Calculate ASEAN average
+## Calculate ASEAN average and ranks
 
 ## Output Thai data
